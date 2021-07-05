3431c053862044c2837ff878897682696d1e1296
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-05 21:01:31 +0300

    changed strings, added statistic setOnClickListener

--- ManagementApp/app/src/main/java/com/example/managementapp/MainActivity.java
+++ ManagementApp/app/src/main/java/com/example/managementapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextClock;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout sendMessageLayoutRef;
+    private LinearLayout statisticLayoutRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sendMessageLayoutRef = findViewById(R.id.sendMessageLayout);
+        statisticLayoutRef = findViewById(R.id.phoneStatisticsLayout);
+
 
         sendMessageLayoutRef.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO HANDLE RETURN WITH TOAST
                 Intent intent = new Intent(MainActivity.this , MessageActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        statisticLayoutRef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , StatisticActivity.class);
                 v.getContext().startActivity(intent);
             }
         });
