6b628f39cf223ba89652cefb5cb7ede8fd0072da
Hodjy <62711261+Hodjy@users.noreply.github.com>
2021-07-10 18:25:06 +0300

    Calendar Polish
    properly implemented the calendar

--- ManagementApp/app/src/main/java/com/example/managementapp/CalendarActivity.java
+++ ManagementApp/app/src/main/java/com/example/managementapp/CalendarActivity.java
@@ -3,7 +3,6 @@ package com.example.managementapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.CalendarContract;
@@ -20,32 +19,40 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private String m_dateFormat;
+    private String m_timeFormat;
+    private SimpleDateFormat m_sdf;
+    private final Calendar m_instancedCalendar = Calendar.getInstance();
     private String m_date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        CalendarView calendar = findViewById(R.id.calendar_inputCalendar);
-        TimePicker timePicker = findViewById(R.id.calendar_timepicker);
+
+        m_dateFormat = getResources().getString(R.string.dateFormatting);
+        m_timeFormat = getResources().getString(R.string.timeFormatting);
+        m_sdf = new SimpleDateFormat(m_dateFormat, Locale.getDefault());
+
+        CalendarView calendarCV = findViewById(R.id.calendar_inputCalendar);
+        TimePicker timePicker = findViewById(R.id.calendar_timepickerStartTime);
         EditText eventTitleET = findViewById(R.id.calendar_eventTitleET);
         EditText eventContentsET = findViewById(R.id.calendar_eventContentsET);
         Button saveBtn = findViewById(R.id.calendar_saveBtn);
 
-        //TODO: Change the format
-        //date initialization
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        m_date = sdf.format(new Date(calendar.getDate()));
-        //
 
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        m_date =  m_sdf.format(new Date(calendarCV.getDate()));
+
+        calendarCV.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                m_date = String.format("%04d/%02d/%02d", year, month, dayOfMonth);
-                //m_date = year + "/" + month + "/" + dayOfMonth;
-                Toast.makeText(CalendarActivity.this, m_date, Toast.LENGTH_LONG).show();
+                m_instancedCalendar.set(year,month,dayOfMonth);
+                m_date = m_sdf.format(m_instancedCalendar.getTime());
             }
         });
 
@@ -56,33 +63,19 @@ public class CalendarActivity extends AppCompatActivity {
             {
                 String eventTitleText = eventTitleET.getText().toString();
                 String eventContentText = eventContentsET.getText().toString();
-                String time = String.format("%02d:%02d:00", timePicker.getHour() , timePicker.getMinute());
-                //String time = timePicker.getHour() + ":" + timePicker.getMinute() + yyyy/MM/dd":00";
+                long millis = getUserSelectedTimeInMillis(timePicker, calendarCV);
 
-                m_date = m_date + " " + time;
-                LocalDateTime ldt = LocalDateTime.parse(m_date, DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-                long millis = ldt.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
-
-                if(!eventTitleText.isEmpty() && !eventContentText.isEmpty() && !m_date.isEmpty())
+                if(!eventTitleText.isEmpty() && !eventContentText.isEmpty())
                 {
                     Intent intent = new Intent(Intent.ACTION_INSERT);
-                    if(true) //If you have the app.
-                    {
-                        intent.setData(CalendarContract.Events.CONTENT_URI);
-                        intent.putExtra(CalendarContract.Events.TITLE , eventTitleText);
-                        intent.putExtra(CalendarContract.Events.DESCRIPTION , eventContentText);
-                        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "");
-                        intent.putExtra(CalendarContract.Events.ALL_DAY, true);
-                        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, millis);
+                    intent.setData(CalendarContract.Events.CONTENT_URI);
+                    intent.putExtra(CalendarContract.Events.TITLE , eventTitleText);
+                    intent.putExtra(CalendarContract.Events.DESCRIPTION , eventContentText);
+                    intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "");
+                    intent.putExtra(CalendarContract.Events.ALL_DAY, false);
+                    intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, millis);
 
-
-
-                        startActivity(intent);
-                    }
-                    else
-                    {
-                        Toast.makeText(CalendarActivity.this, "Theres no app that can support this action.", Toast.LENGTH_LONG).show();
-                    }
+                    startActivity(intent);
                 }
                 else
                 {
@@ -92,4 +85,12 @@ public class CalendarActivity extends AppCompatActivity {
         });
     }
 
+    private long getUserSelectedTimeInMillis(TimePicker timePicker, CalendarView calendar) {
+        String time = String.format(Locale.getDefault(),"%02d:%02d", timePicker.getHour() , timePicker.getMinute());
+        String dateToConvert = m_date + " " + time;
+        LocalDateTime ldt = LocalDateTime.parse(dateToConvert, DateTimeFormatter.ofPattern(m_dateFormat + " " + m_timeFormat));
+        long millis = ldt.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+
+        return millis;
+    }
 }
