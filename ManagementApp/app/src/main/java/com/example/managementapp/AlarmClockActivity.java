29f0a8ae669c1b42dae0828c4b3eda4cc247f176
Hodjy <62711261+Hodjy@users.noreply.github.com>
2021-07-11 16:31:03 +0300

    AlarmClock Activity update
    In addition, added to manifest a permission line.
    Splitted the shapes from circular_btn_style to two files.

--- ManagementApp/app/src/main/java/com/example/managementapp/AlarmClockActivity.java
+++ ManagementApp/app/src/main/java/com/example/managementapp/AlarmClockActivity.java
@@ -1,14 +1,128 @@
 package com.example.managementapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
 
 public class AlarmClockActivity extends AppCompatActivity {
+
+    final int SAVE_ALARM_CLOCK_REQUEST = 1;
+    private String m_noAlarmSavePermission;
+
+    //declared as global for ease of class use:
+    private TimePicker m_TimePicker;
+    private ToggleButton m_Day1Tb;
+    private ToggleButton m_Day2Tb;
+    private ToggleButton m_Day3Tb;
+    private ToggleButton m_Day4Tb;
+    private ToggleButton m_Day5Tb;
+    private ToggleButton m_Day6Tb;
+    private ToggleButton m_Day7Tb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_clock);
+
+        Button saveBtn = findViewById(R.id.alarm_clock_save_btn);
+
+        m_noAlarmSavePermission = getResources().getString(R.string.alarm_clock_no_save_permissions);
+        m_TimePicker = findViewById(R.id.alarm_clock_time_picker);
+        m_Day1Tb = findViewById(R.id.alarm_clock_day1_btn);
+        m_Day2Tb = findViewById(R.id.alarm_clock_day2_btn);
+        m_Day3Tb = findViewById(R.id.alarm_clock_day3_btn);
+        m_Day4Tb = findViewById(R.id.alarm_clock_day4_btn);
+        m_Day5Tb = findViewById(R.id.alarm_clock_day5_btn);
+        m_Day6Tb = findViewById(R.id.alarm_clock_day6_btn);
+        m_Day7Tb = findViewById(R.id.alarm_clock_day7_btn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hasPermissions;
+                hasPermissions = checkSelfPermission(Manifest.permission.SET_ALARM);
+
+                if (Build.VERSION.SDK_INT >= 23)
+                {
+
+                    if(hasPermissions == PackageManager.PERMISSION_GRANTED)
+                    {
+                        saveAlarmClock(m_TimePicker, m_Day1Tb, m_Day2Tb, m_Day3Tb, m_Day4Tb, m_Day5Tb, m_Day6Tb, m_Day7Tb);
+                    }
+                    else
+                    {
+                        requestPermissions(new String[]{Manifest.permission.SET_ALARM}, SAVE_ALARM_CLOCK_REQUEST);
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        });
+    }
+
+    private void saveAlarmClock(TimePicker timePicker, ToggleButton day1Tb, ToggleButton day2Tb, ToggleButton day3Tb, ToggleButton day4Tb, ToggleButton day5Tb, ToggleButton day6Tb, ToggleButton day7Tb) {
+        int hour, minutes;
+        ArrayList<Integer> days;
+        hour = timePicker.getHour();
+        minutes = timePicker.getMinute();
+        days = getDaysForAlarm(day1Tb, day2Tb, day3Tb, day4Tb,
+                day5Tb, day6Tb, day7Tb);
+
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        intent.putExtra(AlarmClock.EXTRA_HOUR, hour);
+        intent.putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        intent.putExtra(AlarmClock.EXTRA_DAYS, days);
+
+        startActivity(intent);
+    }
+
+    private ArrayList<Integer> getDaysForAlarm(ToggleButton day1, ToggleButton day2,
+                                               ToggleButton day3, ToggleButton day4,
+                                               ToggleButton day5, ToggleButton day6,
+                                               ToggleButton day7)
+    {
+        ArrayList<Integer> days = new ArrayList<Integer>();
+
+        days.add(day1.isChecked() ? 1 : 0);
+        days.add(day2.isChecked() ? 1 : 0);
+        days.add(day3.isChecked() ? 1 : 0);
+        days.add(day4.isChecked() ? 1 : 0);
+        days.add(day5.isChecked() ? 1 : 0);
+        days.add(day6.isChecked() ? 1 : 0);
+        days.add(day7.isChecked() ? 1 : 0);
+
+        return days;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == SAVE_ALARM_CLOCK_REQUEST)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                saveAlarmClock(m_TimePicker, m_Day1Tb, m_Day2Tb, m_Day3Tb, m_Day4Tb, m_Day5Tb, m_Day6Tb, m_Day7Tb);
+            }
+            else
+            {
+                Toast.makeText(this, m_noAlarmSavePermission, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
