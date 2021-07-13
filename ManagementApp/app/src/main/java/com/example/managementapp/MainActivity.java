0de66ae9884d7a140b7c3bd54e38cc383899b512
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-13 14:06:12 +0300

    Implemented userEvents with out thread for now

--- ManagementApp/app/src/main/java/com/example/managementapp/MainActivity.java
+++ ManagementApp/app/src/main/java/com/example/managementapp/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.managementapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextClock;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    final int READ_PERMISSION_REQUEST = 1;
+    List<UserEvent> userEventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +45,34 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout settingsLayoutRef = findViewById(R.id.settingsLayout);
         LinearLayout addContactsLayoutRef = findViewById(R.id.addContactLayout);
 
+
+
+        //RecyclerView//
+
+        userEventList = new ArrayList<>();
+
         RecyclerView eventsRecyclerView = findViewById(R.id.main_activity_eventsRecyclerView);
         eventsRecyclerView.setHasFixedSize(true);
 
-        eventsRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        eventsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //eventsRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+
+        //TODO get the userEvents data and put it on this list
+
+        int hasCallPermission = checkSelfPermission(Manifest.permission.READ_CALENDAR);
+
+        if(hasCallPermission == PackageManager.PERMISSION_GRANTED){
+            userEventList = extractUserEventsFromCalendar(this);
+        }
+        else{
+            requestPermissions(new String []{Manifest.permission.READ_CALENDAR}, READ_PERMISSION_REQUEST);
+        }
+
+
+        UserEventAdapter userEventAdapter = new UserEventAdapter(userEventList);
+        eventsRecyclerView.setAdapter(userEventAdapter);
+        //RecyclerView//
 
         sendMessageLayoutRef.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,4 +141,57 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    public List<UserEvent> extractUserEventsFromCalendar(Context context) {
+
+        ContentResolver contentResolver = context.getContentResolver();
+        List<UserEvent> userEvents= new ArrayList<>();
+
+        //selection build
+        long now = new Date().getTime();
+        String nowTime = String.valueOf(now);
+        String threeYearsFromNow = String.valueOf(now + (DateUtils.DAY_IN_MILLIS * 1095));
+        String selection = "((" + CalendarContract.Events.DTSTART + ">" + nowTime +") AND ("
+                +CalendarContract.Events.DTEND + "<" + threeYearsFromNow + "))";
+
+        Cursor cursor = contentResolver.query(CalendarContract.Events.CONTENT_URI,new String[]{
+                CalendarContract.Events.TITLE,
+                CalendarContract.Events.DESCRIPTION,
+                CalendarContract.Events.DTSTART,
+                CalendarContract.Events.DTEND},selection, null, CalendarContract.Events.DTSTART + " ASC");
+
+        if(cursor.moveToFirst())
+        {
+            do{
+                String title = cursor.getString(0);
+                String description =cursor.getString(1);
+                Date dstart = new Date(cursor.getLong(2));
+
+                userEvents.add(new UserEvent(title, description, dstart));
+
+            }while (cursor.moveToNext());
+        }
+
+
+        return userEvents;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @Nullable String[] permissions, @Nullable int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == READ_PERMISSION_REQUEST)
+        {
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                userEventList = extractUserEventsFromCalendar(MainActivity.this);
+            }
+            else{
+                requestPermissions(new String []{Manifest.permission.READ_CALENDAR}, READ_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+
+
 }
