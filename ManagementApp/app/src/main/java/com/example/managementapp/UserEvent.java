6ea2c28c4db369b6723c8e0c8edb076d00ae8d4c
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-21 14:46:29 +0300

    Minor changes
    syntax

--- ManagementApp/app/src/main/java/com/example/managementapp/UserEvent.java
+++ ManagementApp/app/src/main/java/com/example/managementapp/UserEvent.java
@@ -8,33 +8,33 @@ public class UserEvent {
     private String m_Description;
     private String m_Date;
 
-    public UserEvent(String m_Title, String m_Description, String m_Date) {
-        this.m_Title = m_Title;
-        this.m_Description = m_Description;
-        this.m_Date = m_Date;
+    public UserEvent(String i_Title, String i_Description, String i_Date) {
+        m_Title = i_Title;
+        m_Description = i_Description;
+        m_Date = i_Date;
     }
 
     public String getTitle() {
         return m_Title;
     }
 
-    public void setTitle(String m_Title) {
-        this.m_Title = m_Title;
+    public void setTitle(String i_Title) {
+        m_Title = i_Title;
     }
 
     public String getDescription() {
         return m_Description;
     }
 
-    public void setDescription(String m_Description) {
-        this.m_Description = m_Description;
+    public void setDescription(String i_Description) {
+        m_Description = i_Description;
     }
 
     public String getDate() {
         return m_Date;
     }
 
-    public void setDate(String m_Date) {
-        this.m_Date = m_Date;
+    public void setDate(String i_Date) {
+        m_Date = i_Date;
     }
 }
